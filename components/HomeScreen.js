6254cdaf0059b881563f0e549d43448bf826206b
Nguyen Dat <nguyendat03091997@gmail.com>
2019-05-06 20:57:27 +0700

    Third commit

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -8,10 +8,10 @@ export default class Home extends Component{
                 <Text style = {{fontSize : 30, fontWeight : 'bold'}}>This is screen count</Text>
                 <View style = {{flexDirection : 'row', marginTop : 30}}>
                     <TouchableOpacity style = {{height : 40, width : 120, borderRadius : 10 ,backgroundColor : 'green', justifyContent : 'center', alignItems : 'center'}}>
-                        <Text style = {{fontSize : 20, fontWeight : 'bold'}}>Increase</Text>
+                        <Text style = {{fontSize : 20, fontWeight : 'bold', color : 'white'}}>Increase</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style = {{height : 40, width : 120,marginLeft : 15,borderRadius : 10 ,backgroundColor : 'red', justifyContent : 'center', alignItems : 'center'}}>
-                        <Text style = {{fontSize : 20, fontWeight : 'bold'}}>Decrease</Text>
+                        <Text style = {{fontSize : 20, fontWeight : 'bold', color : 'white'}}>Decrease</Text>
                     </TouchableOpacity>
                 </View>
                 <Text style = {{fontSize : 20, fontWeight : 'bold', marginTop : 30}}>Count : 0</Text>
